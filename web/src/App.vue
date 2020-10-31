6f2812b42c333ca853c7cf4703852a5d4a0a6c10
Paulo Lins <paulosilas02@hotmail.com>
2020-10-31 13:01:16 -0300

    Create website manejo

---
+++ web/src/App.vue
@@ -0,0 +1,33 @@
+<template>
+  <div class="layout">
+    <div class="header">
+        <a href="/">
+          <q-avatar rounded>
+            <img src="./assets/logo_manejo.png">
+          </q-avatar>
+        </a>
+        <q-space/>
+          <q-btn class="btn_menu" flat padding="10px 50px" label="home" to="/" />
+          <q-btn class="btn_menu" flat padding="10px 50px" label="Trilhas" to="/trilhas" />
+          <q-btn class="btn_menu" flat padding="10px 50px" label="Projetos" to="/" />
+          <q-btn class="btn_menu" flat padding="10px 50px" label="Sobre" to="/" />
+          <q-btn class="btn_menu" flat padding="10px 50px" label="Contato" to="/" />
+        <q-space/>
+        <q-btn flat padding="8px 10px" label="Entrar" to="/" />
+    </div>
+    <br>
+    <div class="router">
+      <router-view />
+    </div>
+  </div>
+</template>
+<script>
+  require('./styles/layout.css');
+export default {
+  name: 'LayoutDefault',
+  components: {
+
+  }
+}
+</script>
+
